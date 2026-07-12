f825da139162df4fcac5ef509527cdce24016ab3
hiroshi375 <68233466+hiroshi375@users.noreply.github.com>
2026-07-12 20:09:12 +0900

    ステータスバー表示対応

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import "./src/lib/configureAmplify";
 
 import { Authenticator } from "@aws-amplify/ui-react-native";
+import { StatusBar } from "expo-status-bar";
 
 import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
     return (
         <Authenticator.Provider>
+            <StatusBar style="dark" hidden={false} />
+
             <Authenticator>
                 <RootNavigator />
             </Authenticator>
